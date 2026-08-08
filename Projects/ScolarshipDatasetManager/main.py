2936e2ef459c7f4c14345f182448be863e7261b4
Amayl97 <codeamayl97@gmail.com>
2026-08-08 19:09:28 +0500

    Created feature of adding new scholarship

--- Projects/ScolarshipDatasetManager/main.py
+++ Projects/ScolarshipDatasetManager/main.py
@@ -1,7 +1,12 @@
 import pandas as pd
 from modules.search import search_menu
+from modules.add_scholarship import add_scholarships
 
 df = pd.read_csv("data/scholarship.csv")
 
-search_menu(df)
+# search_menu(df)
 
+df = add_scholarships(df)
+df.to_csv("data/scholarship.csv", index=False)
+
+print(df)
---
+++ Projects/ScolarshipDatasetManager/modules/add_scholarship.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+
+
+def add_scholarships(df):
+    print("\nAdd New Scholarship")
+
+    name = input("Name: ")
+    country = input("Country: ")
+    degree = input("Degree: ")
+    deadline = input("Deadline (YYYY-MM-DD): ")
+    cgpa = float(input("Minimum CGPA: "))
+    funded = input("Fully Funded (Yes/No): ")
+    field = input("Field: ")
+
+    new_scholarship = {
+        "Name" :name,
+        "Country": country,
+        "Degree": degree,
+        "Deadline": deadline,
+        "CGPA": cgpa,
+        "FullyFunded": funded,
+        "Field": field
+    }
+
+    df.loc[len(df)] = new_scholarship
+    print("\nScholarship added successfully!")
+
+    return df
